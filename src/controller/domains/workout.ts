f1ab27dd4f8ecd9dffdeb3801c5d38b7e4ba7f21
dgoudie <34222208+dgoudie@users.noreply.github.com>
2022-03-31 12:16:12 -0500

    refactored ws event handlers

--- src/controller/domains/workout.ts
+++ src/controller/domains/workout.ts
@@ -1,10 +1,12 @@
 import { IWorkout, WSWorkoutUpdate } from '@dgoudie/isometric-types';
 import {
-    discardWorkout,
-    endWorkout,
-    getActiveWorkout,
-    persistExercise,
-    startWorkout,
+  discardWorkout,
+  endWorkout,
+  getActiveWorkout,
+  persistSetComplete,
+  persistSetRepetitions,
+  persistSetResistance,
+  startWorkout,
 } from '../../database/domains/workout';
 
 import WebSocket from 'ws';
@@ -15,52 +17,69 @@ import ws from 'express-ws';
 let wsInstance: ws.Instance;
 
 export const initWorkout = (app: ws.Application, instance: ws.Instance) => {
-    wsInstance = instance;
-    app.ws('/api/workout/ws', function (ws, req) {
-        const userId = getUserId(req);
-        //@ts-ignore
-        ws.id = userId;
+  wsInstance = instance;
+  app.ws('/api/workout/ws', function (ws, req) {
+    const userId = getUserId(req);
+    //@ts-ignore
+    ws.id = userId;
 
-        ws.onmessage = handleMessage;
+    ws.onmessage = handleMessage;
 
-        getActiveWorkout(userId).then((workout) => {
-            ws.send(JSON.stringify(workout));
-        });
+    getActiveWorkout(userId).then((workout) => {
+      ws.send(JSON.stringify(workout));
     });
+  });
 };
 
 const handleMessage = async (event: WebSocket.MessageEvent) => {
-    try {
-        //@ts-ignore
-        const userId = event.target.id;
-        const eventPayload: WSWorkoutUpdate = JSON.parse(event.data as string);
-        if (eventPayload.type === 'START') {
-            const workout = await startWorkout(userId);
-            broadcastWorkoutUpdate(userId, workout);
-        } else if (eventPayload.type === 'PERSIST_EXERCISE') {
-            const workout = await persistExercise(
-                userId,
-                eventPayload.exerciseIndex,
-                eventPayload.exercise
-            );
-            broadcastWorkoutUpdate(userId, workout);
-        } else if (eventPayload.type === 'END') {
-            await endWorkout(userId);
-            broadcastWorkoutUpdate(userId, null);
-        } else if (eventPayload.type === 'DISCARD') {
-            await discardWorkout(userId);
-            broadcastWorkoutUpdate(userId, null);
-        }
-    } catch (e) {
-        getLogger().error(e);
+  try {
+    //@ts-ignore
+    const userId = event.target.id;
+    const eventPayload: WSWorkoutUpdate = JSON.parse(event.data as string);
+    if (eventPayload.type === 'START') {
+      const workout = await startWorkout(userId);
+      broadcastWorkoutUpdate(userId, workout);
+    } else if (eventPayload.type === 'PERSIST_SET_COMPLETE') {
+      const workout = await persistSetComplete(
+        userId,
+        eventPayload.exerciseIndex,
+        eventPayload.setIndex,
+        eventPayload.complete
+      );
+      broadcastWorkoutUpdate(userId, workout);
+    } else if (eventPayload.type === 'PERSIST_SET_REPETITIONS') {
+      const workout = await persistSetRepetitions(
+        userId,
+        eventPayload.exerciseIndex,
+        eventPayload.setIndex,
+        eventPayload.repetitions
+      );
+      broadcastWorkoutUpdate(userId, workout);
+    } else if (eventPayload.type === 'PERSIST_SET_RESISTANCE') {
+      const workout = await persistSetResistance(
+        userId,
+        eventPayload.exerciseIndex,
+        eventPayload.setIndex,
+        eventPayload.resistanceInPounds
+      );
+      broadcastWorkoutUpdate(userId, workout);
+    } else if (eventPayload.type === 'END') {
+      await endWorkout(userId);
+      broadcastWorkoutUpdate(userId, null);
+    } else if (eventPayload.type === 'DISCARD') {
+      await discardWorkout(userId);
+      broadcastWorkoutUpdate(userId, null);
     }
+  } catch (e) {
+    getLogger().error(e);
+  }
 };
 
 export const broadcastWorkoutUpdate = (
-    userId: string,
-    workout: IWorkout | null
+  userId: string,
+  workout: IWorkout | null
 ) =>
-    Array.from(wsInstance.getWss().clients)
-        //@ts-ignore
-        .filter((client) => client.id === userId)
-        .forEach((client) => client.send(JSON.stringify(workout)));
+  Array.from(wsInstance.getWss().clients)
+    //@ts-ignore
+    .filter((client) => client.id === userId)
+    .forEach((client) => client.send(JSON.stringify(workout)));
--- src/database/domains/workout.ts
+++ src/database/domains/workout.ts
@@ -1,130 +1,193 @@
 import {
-    IExercise,
-    IWorkoutExercise,
-    IWorkoutExerciseSet,
+  IWorkoutExercise,
+  IWorkoutExerciseSet,
 } from '@dgoudie/isometric-types';
 
 import Workout from '../models/workout';
 import { getNextDaySchedule } from './schedule';
 
 export function getActiveWorkout(userId: string) {
-    return Workout.findOne({ userId, endedAt: undefined });
+  return Workout.findOne({ userId, endedAt: undefined });
 }
 
 export async function startWorkout(userId: string) {
-    const { nickname, dayNumber, exercises } = await getNextDaySchedule(userId);
+  const { nickname, dayNumber, exercises } = await getNextDaySchedule(userId);
 
-    let exercisesMapped: IWorkoutExercise[] = exercises.map((exercise) => ({
-        exerciseId: exercise._id,
-        sets: new Array<IWorkoutExerciseSet>(exercise.setCount).fill({
-            complete: false,
-        }),
-    }));
+  let exercisesMapped: IWorkoutExercise[] = exercises.map((exercise) => ({
+    exerciseId: exercise._id,
+    sets: new Array<IWorkoutExerciseSet>(exercise.setCount).fill({
+      complete: false,
+    }),
+  }));
 
-    const alreadyInProgressWorkout = await Workout.findOne({
-        userId,
-        endedAt: undefined,
-    });
+  const alreadyInProgressWorkout = await Workout.findOne({
+    userId,
+    endedAt: undefined,
+  });
 
-    if (!!alreadyInProgressWorkout) {
-        return alreadyInProgressWorkout;
-    }
+  if (!!alreadyInProgressWorkout) {
+    return alreadyInProgressWorkout;
+  }
 
-    return Workout.create({
-        userId,
-        startedAt: new Date(),
-        dayNumber,
-        nickname,
-        exercises: exercisesMapped,
-    });
+  return Workout.create({
+    userId,
+    startedAt: new Date(),
+    dayNumber,
+    nickname,
+    exercises: exercisesMapped,
+  });
 }
 
-export async function persistExercise(
-    userId: string,
-    exerciseIndex: number,
-    exercise: IWorkoutExercise
+export async function persistSetComplete(
+  userId: string,
+  exerciseIndex: number,
+  setIndex: number,
+  complete: boolean
 ) {
-    await normalizeExercise(exercise, exerciseIndex, userId);
-    await Workout.updateOne(
-        { userId, endedAt: undefined },
-        { $set: { [`exercises.${exerciseIndex}`]: exercise } }
-    );
-    return getActiveWorkout(userId);
+  await Workout.updateOne(
+    { userId, endedAt: undefined },
+    {
+      $set: {
+        [`exercises.${exerciseIndex}.sets.${setIndex}.complete`]: complete,
+      },
+    }
+  );
+  await ensureNoIncompleteSetsBeforeCompleteSets(userId, exerciseIndex);
+  return getActiveWorkout(userId);
+}
+
+export async function persistSetRepetitions(
+  userId: string,
+  exerciseIndex: number,
+  setIndex: number,
+  repetitions: number | undefined
+) {
+  await Workout.updateOne(
+    { userId, endedAt: undefined },
+    {
+      [typeof repetitions !== 'undefined' ? '$set' : '$unset']: {
+        [`exercises.${exerciseIndex}.sets.${setIndex}.repetitions`]:
+          repetitions ?? '',
+      },
+    }
+  );
+  return getActiveWorkout(userId);
+}
+
+export async function persistSetResistance(
+  userId: string,
+  exerciseIndex: number,
+  setIndex: number,
+  resistanceInPounds: number | undefined
+) {
+  await Workout.updateOne(
+    { userId, endedAt: undefined },
+    {
+      [typeof resistanceInPounds !== 'undefined' ? '$set' : '$unset']: {
+        [`exercises.${exerciseIndex}.sets.${setIndex}.resistanceInPounds`]:
+          resistanceInPounds ?? '',
+      },
+    }
+  );
+  await populateResistanceForNextSets(userId, exerciseIndex, setIndex);
+  return getActiveWorkout(userId);
 }
 
 export async function endWorkout(userId: string) {
-    return Workout.updateOne(
-        {
-            userId,
-            endedAt: undefined,
-        },
-        { endedAt: new Date() }
-    );
+  return Workout.updateOne(
+    {
+      userId,
+      endedAt: undefined,
+    },
+    { endedAt: new Date() }
+  );
 }
 
 export async function discardWorkout(userId: string) {
-    return Workout.deleteOne({
-        userId,
-        endedAt: undefined,
-    });
+  return Workout.deleteOne({
+    userId,
+    endedAt: undefined,
+  });
 }
 
 export function getMostRecentCompletedWorkout(userId: string) {
-    return Workout.findOne({
-        userId,
-        endedAt: { $exists: true },
-    }).sort({ createdAt: -1 });
+  return Workout.findOne({
+    userId,
+    endedAt: { $exists: true },
+  }).sort({ createdAt: -1 });
 }
 
-async function normalizeExercise(
-    exercise: IWorkoutExercise,
-    exerciseIndex: number,
-    userId: string
+async function ensureNoIncompleteSetsBeforeCompleteSets(
+  userId: string,
+  exerciseIndex: number
 ) {
-    ensureNoIncompleteSetsBeforeCompleteSets(exercise);
-    await populateResistanceForNextSets(exercise, exerciseIndex, userId);
-}
-function ensureNoIncompleteSetsBeforeCompleteSets(exercise: IWorkoutExercise) {
-    exercise.sets.forEach((set, index) => {
-        if (!set.complete) {
-            let nextSet = exercise.sets[index + 1];
-            nextSet && (nextSet.complete = false);
-        }
-    });
+  let activeWorkout = await getActiveWorkout(userId);
+  if (!activeWorkout) {
+    return;
+  }
+  let exerciseAtIndex = activeWorkout.exercises[exerciseIndex];
+  let firstUnfinishedExercise = exerciseAtIndex.sets.findIndex(
+    (set) => !set.complete
+  );
+  if (firstUnfinishedExercise < 0) {
+    return;
+  }
+  let updatePromises = [];
+  for (let i = firstUnfinishedExercise; i < exerciseAtIndex.sets.length; i++) {
+    updatePromises.push(
+      Workout.updateOne(
+        {
+          userId,
+          endedAt: undefined,
+        },
+        { $set: { [`exercises.${exerciseIndex}.sets.${i}.complete`]: false } }
+      )
+    );
+  }
+  await Promise.all(updatePromises);
 }
 async function populateResistanceForNextSets(
-    exercise: IWorkoutExercise,
-    exerciseIndex: number,
-    userId: string
+  userId: string,
+  exerciseIndex: number,
+  setIndex: number
 ) {
-    let activeWorkout = await getActiveWorkout(userId);
-    if (!activeWorkout) {
-        return;
+  if (setIndex !== 0) {
+    return;
+  }
+  let activeWorkout = await getActiveWorkout(userId);
+  if (!activeWorkout) {
+    return;
+  }
+  let exerciseAtIndex = activeWorkout.exercises[exerciseIndex];
+  let anyOtherExercisesAlreadyHaveResistanceSet = !exerciseAtIndex.sets
+    .slice(1)
+    .every((set) => typeof set.resistanceInPounds === 'undefined');
+  if (anyOtherExercisesAlreadyHaveResistanceSet) {
+    return;
+  }
+  if (exerciseAtIndex.sets.length < 2) {
+    return;
+  }
+  const firstSetResistance = exerciseAtIndex.sets[0].resistanceInPounds;
+  const everyOtherSetResistanceIsUndefined = exerciseAtIndex.sets
+    .slice(1)
+    .every((set) => typeof set.resistanceInPounds === 'undefined');
+  if (
+    typeof firstSetResistance === 'undefined' ||
+    !everyOtherSetResistanceIsUndefined
+  ) {
+    return;
+  }
+  await Workout.updateOne(
+    {
+      userId,
+      endedAt: undefined,
+    },
+    {
+      $set: {
+        [`exercises.${exerciseIndex}.sets.$[].resistanceInPounds`]:
+          firstSetResistance,
+      },
     }
-    let exerciseAtIndex = activeWorkout.exercises[exerciseIndex];
-    let anyExercisesAlreadyHaveResistanceSet = !exerciseAtIndex.sets.every(
-        (set) => typeof set.resistanceInPounds === 'undefined'
-    );
-    if (anyExercisesAlreadyHaveResistanceSet) {
-        return;
-    }
-    if (exercise.sets.length < 2) {
-        return;
-    }
-    const firstSetResistance = exercise.sets[0].resistanceInPounds;
-    const everyOtherSetResistanceIsUndefined = exercise.sets
-        .slice(1)
-        .every((set) => typeof set.resistanceInPounds === 'undefined');
-    if (
-        typeof firstSetResistance === 'undefined' ||
-        !everyOtherSetResistanceIsUndefined
-    ) {
-        return;
-    }
-    exercise.sets.forEach((set, index) => {
-        if (index === 0) {
-            return;
-        }
-        set.resistanceInPounds = firstSetResistance;
-    });
+  );
 }
