ebf1deb9f8e8826e7772974e5f7b4971440f9bc4
dgoudie <34222208+dgoudie@users.noreply.github.com>
2022-04-02 19:34:43 -0500

    added new big pipeline for exercises

--- src/database/aggregations.ts
+++ src/database/aggregations.ts
@@ -1,290 +1,242 @@
+import mongoose, { PipelineStage } from 'mongoose';
+
+import { ExerciseMuscleGroup } from '@dgoudie/isometric-types';
+
 export const buildNextDayScheduleAggregation = (nextDayNumber: number) => {
-    let pipeline: object[] = [];
-    pipeline = [
-        {
-            $unwind: {
-                path: '$days',
-                includeArrayIndex: 'dayNumber',
-            },
-        },
-        {
-            $project: {
-                _id: '$days._id',
-                nickname: '$days.nickname',
-                exercises: '$days.exercises',
-                dayNumber: '$dayNumber',
-            },
-        },
-        {
-            $match: {
-                $or: [
-                    {
-                        dayNumber: nextDayNumber,
-                    },
-                    {
-                        dayNumber: 0,
-                    },
-                ],
-            },
-        },
-        {
-            $sort: {
-                dayNumber: -1,
-            },
-        },
-        {
-            $limit: 1,
-        },
-    ];
-    return pipeline;
+  let pipeline: PipelineStage[] = [];
+  pipeline = [
+    {
+      $unwind: {
+        path: '$days',
+        includeArrayIndex: 'dayNumber',
+      },
+    },
+    {
+      $project: {
+        _id: '$days._id',
+        nickname: '$days.nickname',
+        exercises: '$days.exercises',
+        dayNumber: '$dayNumber',
+      },
+    },
+    {
+      $match: {
+        $or: [
+          {
+            dayNumber: nextDayNumber,
+          },
+          {
+            dayNumber: 0,
+          },
+        ],
+      },
+    },
+    {
+      $sort: {
+        dayNumber: -1,
+      },
+    },
+    {
+      $limit: 1,
+    },
+  ];
+  return pipeline;
 };
 
-// export const buildBeerOrLiquorBrandsByTypeAggregation = (
-//   onlyShowInStock: boolean,
-//   onlyOutOfStock: boolean,
-//   filterText?: string
-// ) => {
-//   let pipeline: object[] = [];
-//   if (onlyShowInStock) {
-//     pipeline = [...pipeline, { $match: { inStock: true } }];
-//   }
-//   if (onlyOutOfStock) {
-//     pipeline = [...pipeline, { $match: { inStock: false } }];
-//   }
-//   if (!!filterText) {
-//     pipeline = [
-//       ...pipeline,
-//       {
-//         $match: { $expr: buildAndQueryForText(filterText, '$nameNormalized') },
-//       },
-//     ];
-//   }
-//   pipeline = [
-//     ...pipeline,
-//     {
-//       $project: {
-//         type: 1,
-//         temp: {
-//           _id: '$_id',
-//           name: '$name',
-//           nameNormalized: '$nameNormalized',
-//           inStock: '$inStock',
-//           price: '$price',
-//           type: '$type',
-//         },
-//       },
-//     },
-//     {
-//       $sort: {
-//         type: 1,
-//         'temp.name': 1,
-//       },
-//     },
-//     {
-//       $group: {
-//         _id: '$type',
-//         items: {
-//           $push: '$temp',
-//         },
-//       },
-//     },
-//     {
-//       $project: {
-//         type: '$_id',
-//         items: '$items',
-//       },
-//     },
-//     {
-//       $sort: {
-//         type: 1,
-//       },
-//     },
-//     {
-//       $unset: '_id',
-//     },
-//   ];
-//   return pipeline;
-// };
-
-// export const buildMixedDrinkRecipesWithIngredientsAggregation = (
-//   onlyShowItemsWithAllIngedientsInStock: boolean,
-//   filterText?: string
-// ) => {
-//   let pipeline: object[] = [
-//     {
-//       $lookup: {
-//         from: properties.mongodbBeerOrLiquorBrandsCollectionName,
-//         localField: 'requiredBeersOrLiquors._id',
-//         foreignField: '_id',
-//         as: 'requiredBeersOrLiquorsWithDetail',
-//       },
-//     },
-//     {
-//       $addFields: {
-//         requiredBeersOrLiquors: {
-//           $map: {
-//             input: '$requiredBeersOrLiquorsWithDetail',
-//             as: 'r',
-//             in: {
-//               _id: '$$r._id',
-//               name: '$$r.name',
-//               nameNormalized: '$$r.nameNormalized',
-//               additionalNotes: '$$r.additionalNotes',
-//               inStock: '$$r.inStock',
-//               price: '$$r.price',
-//               count: {
-//                 $arrayElemAt: [
-//                   '$requiredBeersOrLiquors.count',
-//                   {
-//                     $indexOfArray: ['$requiredBeersOrLiquors._id', '$$r._id'],
-//                   },
-//                 ],
-//               },
-//             },
-//           },
-//         },
-//       },
-//     },
-//   ];
-//   if (!!onlyShowItemsWithAllIngedientsInStock) {
-//     pipeline = [
-//       ...pipeline,
-//       {
-//         $match: {
-//           requiredBeersOrLiquors: {
-//             $not: {
-//               $elemMatch: {
-//                 inStock: false,
-//               },
-//             },
-//           },
-//         },
-//       },
-//     ];
-//   }
-//   pipeline = [
-//     ...pipeline,
-//     {
-//       $addFields: {
-//         requiredBeersOrLiquors: {
-//           $map: {
-//             input: '$requiredBeersOrLiquors',
-//             as: 'row',
-//             in: {
-//               _id: '$$row._id',
-//               name: '$$row.name',
-//               inStock: '$$row.inStock',
-//               price: '$$row.price',
-//               count: '$$row.count',
-//               nameNormalized: '$$row.nameNormalized',
-//               additionalNotes: '$$row.additionalNotes',
-//               calculatedPrice: {
-//                 $multiply: ['$$row.price', '$$row.count'],
-//               },
-//             },
-//           },
-//         },
-//       },
-//     },
-//   ];
-//   if (!!filterText) {
-//     pipeline = [
-//       ...pipeline,
-//       {
-//         $addFields: {
-//           requiredBeersOrLiquors: {
-//             $map: {
-//               input: '$requiredBeersOrLiquors',
-//               as: 'row',
-//               in: {
-//                 _id: '$$row._id',
-//                 name: '$$row.name',
-//                 inStock: '$$row.inStock',
-//                 price: '$$row.price',
-//                 count: '$$row.count',
-//                 nameNormalized: '$$row.nameNormalized',
-//                 additionalNotes: '$$row.additionalNotes',
-//                 calculatedPrice: '$$row.calculatedPrice',
-//                 nameMatch: buildAndQueryForText(
-//                   filterText,
-//                   '$$row.nameNormalized'
-//                 ),
-//               },
-//             },
-//           },
-//         },
-//       },
-//     ];
-//   }
-//   pipeline = [
-//     ...pipeline,
-//     {
-//       $addFields: {
-//         price: {
-//           $divide: [
-//             {
-//               $round: {
-//                 $multiply: [
-//                   { $sum: '$requiredBeersOrLiquors.calculatedPrice' },
-//                   2,
-//                 ],
-//               },
-//             },
-//             2,
-//           ],
-//         },
-//       },
-//     },
-//   ];
-//   if (!!filterText) {
-//     pipeline = [
-//       ...pipeline,
-//       {
-//         $match: {
-//           $or: [
-//             {
-//               $expr: buildAndQueryForText(filterText, '$nameNormalized'),
-//             },
-//             {
-//               requiredBeersOrLiquors: {
-//                 $elemMatch: {
-//                   nameMatch: true,
-//                 },
-//               },
-//             },
-//           ],
-//         },
-//       },
-//     ];
-//   }
-//   pipeline = [
-//     ...pipeline,
-//     {
-//       $unset: [
-//         'requiredBeersOrLiquors.calculatedPrice',
-//         'requiredBeersOrLiquors.nameMatch',
-//         'requiredBeersOrLiquorsWithDetail',
-//       ],
-//     },
-//     {
-//       $sort: { nameNormalized: 1 },
-//     },
-//   ];
-//   return pipeline;
-// };
-
-// const buildAndQueryForText = (
-//   text: string,
-//   argName: string
-// ): { $and: { $function: any }[] } => {
-//   const tokenizedText = text.toLowerCase().replace(/\//g, '\\').split(' ');
-//   return {
-//     $and: tokenizedText.map<{ $function: any }>((token) => ({
-//       $function: {
-//         body: `function(text) { return text.includes("${token}")}`,
-//         args: [argName],
-//         lang: 'js',
-//       },
-//     })),
-//   };
-// };
+export const buildFindExercisesWithBasicHistoryQuery = (
+  userId: string,
+  $search?: string,
+  muscleGroup?: ExerciseMuscleGroup,
+  page?: number
+) => {
+  let query: object = { userId: new mongoose.Types.ObjectId(userId) };
+  if (!!$search) {
+    $search = $search.replace(/(\w+)/g, '"$1"');
+    query = {
+      ...query,
+      $text: { $search },
+    };
+  }
+  if (!!muscleGroup) {
+    query = {
+      ...query,
+      $or: [
+        { primaryMuscleGroup: muscleGroup },
+        { secondaryMuscleGroups: muscleGroup },
+      ],
+    };
+  }
+  let pipeline: PipelineStage[] = [
+    {
+      $match: query,
+    },
+    {
+      $lookup: {
+        from: 'workouts',
+        as: 'workouts',
+        let: {
+          exercise_id: '$_id',
+        },
+        pipeline: [
+          {
+            $unwind: '$exercises',
+          },
+          {
+            $match: {
+              $expr: {
+                $eq: ['$exercises.exerciseId', '$$exercise_id'],
+              },
+            },
+          },
+        ],
+      },
+    },
+    {
+      $unwind: {
+        path: '$workouts',
+        preserveNullAndEmptyArrays: true,
+      },
+    },
+    {
+      $addFields: {
+        'workouts.exercises.totalRepsForSet': {
+          $sum: '$workouts.exercises.sets.repetitions',
+        },
+      },
+    },
+    {
+      $unwind: {
+        path: '$workouts.exercises.sets',
+        preserveNullAndEmptyArrays: true,
+        includeArrayIndex: 'workouts.exercises.sets.setIndex',
+      },
+    },
+    {
+      $match: {
+        $or: [
+          {
+            'workouts.exercises.exerciseId': {
+              $exists: false,
+            },
+          },
+          {
+            'workouts.exercises.sets.complete': true,
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        'workouts.exercises.sets.workoutStart': '$workouts.createdAt',
+        'workouts.exercises.sortableValue': {
+          $cond: [
+            {
+              $eq: ['$exercise.exerciseType', 'rep_based'],
+            },
+            '$workouts.exercises.totalRepsForSet',
+            '$workouts.exercises.sets.resistanceInPounds',
+          ],
+        },
+      },
+    },
+    {
+      $sort: {
+        _id: 1,
+        'workouts.exercises.sortableValue': -1,
+      },
+    },
+    {
+      $group: {
+        _id: '$_id',
+        exerciseRoot: {
+          $first: '$$ROOT',
+        },
+        sets: {
+          $push: '$workouts.exercises.sets',
+        },
+        bestSet: {
+          $first: '$workouts.exercises.sets',
+        },
+      },
+    },
+    {
+      $replaceRoot: {
+        newRoot: {
+          $mergeObjects: [
+            '$exerciseRoot',
+            {
+              bestSet: {
+                $cond: [
+                  {
+                    $not: '$bestSet.workoutStart',
+                  },
+                  '$$REMOVE',
+                  '$bestSet',
+                ],
+              },
+              sets: {
+                $filter: {
+                  input: '$sets',
+                  as: 'set',
+                  cond: {
+                    $not: {
+                      $not: '$$set.workoutStart',
+                    },
+                  },
+                },
+              },
+              lastPerformed: {
+                $max: '$exerciseRoot.workouts.exercises.sets.workoutStart',
+              },
+            },
+          ],
+        },
+      },
+    },
+    { $unset: 'workouts' },
+    {
+      $unwind: {
+        path: '$sets',
+        preserveNullAndEmptyArrays: true,
+      },
+    },
+    {
+      $sort: {
+        'sets.workoutStart': 1,
+        'sets.setIndex': 1,
+      },
+    },
+    {
+      $group: {
+        _id: '$_id',
+        _: {
+          $first: '$$ROOT',
+        },
+        sets: {
+          $push: '$sets',
+        },
+      },
+    },
+    {
+      $replaceRoot: {
+        newRoot: {
+          $mergeObjects: [
+            '$_',
+            {
+              sets: '$sets',
+            },
+          ],
+        },
+      },
+    },
+    {
+      $sort: {
+        name: 1,
+      },
+    },
+  ];
+  if (typeof page !== 'undefined') {
+    pipeline = [...pipeline, { $skip: (page - 1) * 10 }, { $limit: 10 }];
+  }
+  return pipeline;
+};
--- src/database/domains/exercise.ts
+++ src/database/domains/exercise.ts
@@ -1,41 +1,24 @@
 import { ExerciseMuscleGroup, IExercise } from '@dgoudie/isometric-types';
 
 import Exercise from '../models/exercise';
+import { buildFindExercisesWithBasicHistoryQuery } from '../aggregations';
 import mongoose from 'mongoose';
 
 export function getExercises(
-    userId: string,
-    $search?: string,
-    muscleGroup?: ExerciseMuscleGroup,
-    page?: number
+  userId: string,
+  $search?: string,
+  muscleGroup?: ExerciseMuscleGroup,
+  page?: number
 ) {
-    let query: mongoose.FilterQuery<IExercise> = { userId };
-    if (!!$search) {
-        $search = $search.replace(/(\w+)/g, '"$1"');
-        query = {
-            ...query,
-            $text: { $search },
-        };
-    }
-    if (!!muscleGroup) {
-        query = {
-            ...query,
-            $or: [
-                { primaryMuscleGroup: muscleGroup },
-                { secondaryMuscleGroups: muscleGroup },
-            ],
-        };
-    }
-    if (typeof page === 'undefined') {
-        return Exercise.find(query).sort({ name: 1 });
-    } else {
-        return Exercise.find(query)
-            .sort({ name: 1 })
-            .limit(10)
-            .skip((page - 1) * 10);
-    }
+  const pipeline = buildFindExercisesWithBasicHistoryQuery(
+    userId,
+    $search,
+    muscleGroup,
+    page
+  );
+  return Exercise.aggregate(pipeline);
 }
 
 export function getExercise(userId: string, name: string) {
-    return Exercise.findOne({ userId, name });
+  return Exercise.findOne({ userId, name });
 }
