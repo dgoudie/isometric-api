1e3c83b60d564aeb15721e3e7fc175ae731795ca
dgoudie <34222208+dgoudie@users.noreply.github.com>
2022-04-07 23:11:22 -0500

    added new models

--- src/database/domains/exercise.ts
+++ src/database/domains/exercise.ts
@@ -1,7 +1,12 @@
-import { ExerciseMuscleGroup, IExercise } from '@dgoudie/isometric-types';
+import {
+  ExerciseMuscleGroup,
+  IExercise,
+  IExerciseWithHistory,
+} from '@dgoudie/isometric-types';
 
 import Exercise from '../models/exercise';
 import { buildFindExercisesWithBasicHistoryQuery } from '../aggregations';
+import mongoose from 'mongoose';
 
 export function getExercises(
   userId: string,
@@ -23,6 +28,52 @@ export function getExerciseById(userId: string, _id: string) {
   return Exercise.findOne({ userId, _id });
 }
 
-export function getExerciseByName(userId: string, name: string) {
-  return Exercise.findOne({ userId, name });
+export async function getExerciseByName(
+  userId: string,
+  name: string
+): Promise<IExerciseWithHistory> {
+  const [exercise] = await Exercise.aggregate([
+    {
+      $match: {
+        name,
+        userId: new mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $lookup: {
+        from: 'workouts',
+        as: 'instances',
+        let: {
+          exerciseId: '$_id',
+        },
+        pipeline: [
+          {
+            $unwind: {
+              path: '$exercises',
+            },
+          },
+          {
+            $match: {
+              $expr: {
+                $and: [
+                  {
+                    userId: new mongoose.Types.ObjectId(userId),
+                  },
+                  {
+                    $eq: ['$exercises.exercise._id', '$$exerciseId'],
+                  },
+                ],
+              },
+            },
+          },
+          {
+            $sort: {
+              createdAt: -1,
+            },
+          },
+        ],
+      },
+    },
+  ]);
+  return exercise;
 }
